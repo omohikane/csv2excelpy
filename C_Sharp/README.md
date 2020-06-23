4553e4a3d791637498b2d356918118598d438d94
y-maru-94 <da.kuyou@gmail.com>
2020-06-23 21:54:52 +0900

    c sharp 追加

---
+++ C_Sharp/README.md
@@ -0,0 +1,38 @@
+# C_Sharp
+
+csv2excelpyのC#バージョン
+
+## Requirement
+
+- Visual Studio 2019  
+- ClosedXML  
+
+```
+.
+├── csvToExcelFile
+│   ├── bin
+│ 　　   ├── Debug
+│     　　   ├── csvToExcelFile.exe
+│     　　   ├── csv
+│     　　   ├── xlsx
+├── packages
+|   ├── 色々必要な
+├── csvToExcelFile.sln
+└── README.md
+```
+
+## Installation
+
+PMコンソールからインストール
+`Install-Package ClosedXML`
+
+## Usage
+試す場合  
+`./csvToExcelFile/bin/Debug/`の中のCSVファイルをxlsxに
+`./csvToExcelFile/bin/Debug/csvToExcelFile.exe`を使う
+
+## Author
+maru-94
+
+## LICENSE
+MIT
---
+++ C_Sharp/csvToExcelFile/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace csvToExcelFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertButton = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(114, 12);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(14, 12);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(94, 23);
+            this.openFileButton.TabIndex = 3;
+            this.openFileButton.Text = "ファイルの選択";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(14, 41);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(551, 164);
+            this.textBox.TabIndex = 4;
+            this.textBox.Text = "読み込むファイル：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 217);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.convertButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
